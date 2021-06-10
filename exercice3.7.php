acd9ec3a43ee2724519cea0490084973b926763b
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-10 10:54:13 +0200

    exercice 3.7 fait

---
+++ exercice3.7.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 3.7</title>
+</head>
+<body>
+    <h1>Exercice 7 Partie 3 : Les boucles PHP</h1>
+    <p>
+    <?php
+    for($firstNumber = 1; $firstNumber <=100; $firstNumber+=15)
+    {
+        echo ('On tient le bon bout! ');
+    }
+    ?>
+    </p>
+</body>
+</html>
